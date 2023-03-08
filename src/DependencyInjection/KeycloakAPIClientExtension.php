8161fef0791bf6b6c9f98fb1e6d2b2a6f697ed89
leon-manq <leon.juraschek@manq.de>
2023-03-08 18:22:30 +0100

    Initial commit

---
+++ src/DependencyInjection/KeycloakAPIClientExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ZoomAPIClientBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+
+class ZoomAPIClientExtension extends Extension
+{
+
+    /**
+     * this function loads the services form the xml file and provides it the symfony service container.
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @return void
+     * @throws \Exception when the services are not found
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configLoader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config/'));
+        $configLoader->load('services.xml');
+    }
+}
---
+++ src/KeycloakApiClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace manQ\KeycloakApiClientBundle;
+
+class KeycloakApiClient
+{
+
+}
